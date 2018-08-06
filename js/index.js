17f2e4aaa6b6089510632a4b756b7a3cac08400f
szincone <sawyerzincone@gmail.com>
2018-08-06 17:25:41 -0600

    got rid of stats reference error

--- js/index.js
+++ js/index.js
@@ -1,56 +1,114 @@
 // init Isotope
-var $grid = $('.grid').isotope({
-    itemSelector: '.element-item',
-    layoutMode: 'fitRows',
-    getSortData: {
-    name: '.name',
-    symbol: '.symbol',
-    number: '.number parseInt',
-    category: '[data-category]',
-    weight: function( itemElem ) {
-    var weight = $( itemElem ).find('.weight').text();
-    return parseFloat( weight.replace( /[\(\)]/g, '') );
+var $grid = $(".grid").isotope({
+  itemSelector: ".element-item",
+  layoutMode: "fitRows",
+  getSortData: {
+    name: ".name",
+    symbol: ".symbol",
+    number: ".number parseInt",
+    category: "[data-category]",
+    weight: function(itemElem) {
+      var weight = $(itemElem)
+        .find(".weight")
+        .text();
+      return parseFloat(weight.replace(/[\(\)]/g, ""));
     }
-}
+  }
 });
 
 // filter functions
 var filterFns = {
-// show if number is greater than 50
-numberGreaterThan50: function() {
-    var number = $(this).find('.number').text();
-    return parseInt( number, 10 ) > 50;
-},
-// show if name ends with -ium
-ium: function() {
-    var name = $(this).find('.name').text();
-    return name.match( /ium$/ );
-}
+  // show if number is greater than 50
+  numberGreaterThan50: function() {
+    var number = $(this)
+      .find(".number")
+      .text();
+    return parseInt(number, 10) > 50;
+  },
+  // show if name ends with -ium
+  ium: function() {
+    var name = $(this)
+      .find(".name")
+      .text();
+    return name.match(/ium$/);
+  }
 };
 
 // bind filter button click
-$('#filters').on( 'click', 'button', function() {
-var filterValue = $( this ).attr('data-filter');
-// use filterFn if matches value
-filterValue = filterFns[ filterValue ] || filterValue;
-$grid.isotope({ filter: filterValue });
+$("#filters").on("click", "button", function() {
+  var filterValue = $(this).attr("data-filter");
+  // use filterFn if matches value
+  filterValue = filterFns[filterValue] || filterValue;
+  $grid.isotope({ filter: filterValue });
 });
 
 // bind sort button click
-$('#sorts').on( 'click', 'button', function() {
-var sortByValue = $(this).attr('data-sort-by');
-$grid.isotope({ sortBy: sortByValue });
+$("#sorts").on("click", "button", function() {
+  var sortByValue = $(this).attr("data-sort-by");
+  $grid.isotope({ sortBy: sortByValue });
 });
 
 // change is-checked class on buttons
-$('.button-group').each( function( i, buttonGroup ) {
-var $buttonGroup = $( buttonGroup );
-$buttonGroup.on( 'click', 'button', function() {
-    $buttonGroup.find('.is-checked').removeClass('is-checked');
-    $( this ).addClass('is-checked');
-});
+$(".button-group").each(function(i, buttonGroup) {
+  var $buttonGroup = $(buttonGroup);
+  $buttonGroup.on("click", "button", function() {
+    $buttonGroup.find(".is-checked").removeClass("is-checked");
+    $(this).addClass("is-checked");
+  });
 });
 
-particlesJS("particles-js", {"particles":{"number":{"value":160,"density":{"enable":true,"value_area":800}},"color":{"value":"#ffffff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":1,"random":true,"anim":{"enable":true,"speed":1,"opacity_min":0,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":4,"size_min":0.3,"sync":false}},"line_linked":{"enable":false,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":1,"direction":"none","random":true,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":600}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"bubble"},"onclick":{"enable":true,"mode":"repulse"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":250,"size":0,"duration":2,"opacity":0,"speed":3},"repulse":{"distance":400,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});var count_particles, stats, update; stats = new Stats; stats.setMode(0); stats.domElement.style.position = 'absolute'; stats.domElement.style.left = '0px'; stats.domElement.style.top = '0px'; document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles'); update = function() { stats.begin(); stats.end(); if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; } requestAnimationFrame(update); }; requestAnimationFrame(update);;
-    
-
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 160, density: { enable: true, value_area: 800 } },
+    color: { value: "#ffffff" },
+    shape: {
+      type: "circle",
+      stroke: { width: 0, color: "#000000" },
+      polygon: { nb_sides: 5 },
+      image: { src: "img/github.svg", width: 100, height: 100 }
+    },
+    opacity: {
+      value: 1,
+      random: true,
+      anim: { enable: true, speed: 1, opacity_min: 0, sync: false }
+    },
+    size: {
+      value: 3,
+      random: true,
+      anim: { enable: false, speed: 4, size_min: 0.3, sync: false }
+    },
+    line_linked: {
+      enable: false,
+      distance: 150,
+      color: "#ffffff",
+      opacity: 0.4,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 1,
+      direction: "none",
+      random: true,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: { enable: false, rotateX: 600, rotateY: 600 }
+    }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: { enable: true, mode: "bubble" },
+      onclick: { enable: true, mode: "repulse" },
+      resize: true
+    },
+    modes: {
+      grab: { distance: 400, line_linked: { opacity: 1 } },
+      bubble: { distance: 250, size: 0, duration: 2, opacity: 0, speed: 3 },
+      repulse: { distance: 400, duration: 0.4 },
+      push: { particles_nb: 4 },
+      remove: { particles_nb: 2 }
+    }
+  },
+  retina_detect: true
+});
